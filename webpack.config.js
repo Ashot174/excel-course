6d24fba9762109abd86f6dcf8485cc1ebe9a8600
Ashot174 <ashot.grigoryan.90@inbox.ru>
2021-05-21 15:25:17 +0400

    Create unit testing env

--- webpack.config.js
+++ webpack.config.js
@@ -14,15 +14,8 @@ console.log('IS PROD', isProd)
 console.log('IS DEV', isDev)
 
 const jsLoaders = () => {
-    const loaders = [
-        {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-                plugins: ['@babel/plugin-proposal-class-properties'],
-            }
-        }
-    ]
+    const loaders = ['babel-loader']
+
     if (isDev) {
         loaders.push('eslint-loader')
     }
